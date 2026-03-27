1fef2e053cd468c00717eef3bf39e5f7d827215e
Ishant Thakkar <ishant.thakkar@metizsoft.com>
2026-03-27 12:33:46 +0530

    change baseurl

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,6 +1,8 @@
 
 
-export const API_BASE_URL = process.env.ASSETS_BASE_URL || "http://localhost:5000/api/";
-export const ASSETS_BASE_URL = process.env.API_BASE_URL ? (process.env.NEXT_PUBLIC_BASE_URL + "/") : "http://localhost:5000/";
+// export const API_BASE_URL = "http://localhost:5000/api/";
+// export const ASSETS_BASE_URL = "http://localhost:5000/";
 
+export const API_BASE_URL = "https://caip-backend-uzjx.onrender.com/api/";
+export const ASSETS_BASE_URL = "https://caip-backend-uzjx.onrender.com/";
 
