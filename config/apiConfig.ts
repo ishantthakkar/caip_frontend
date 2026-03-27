d8de55575fa43313399f823505b1d99a0cf3fdc4
Ishant Thakkar <ishant.thakkar@metizsoft.com>
2026-03-27 12:30:18 +0530

    set env file

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,6 +1,6 @@
 
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/";
-export const ASSETS_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ? (process.env.NEXT_PUBLIC_BASE_URL + "/") : "http://localhost:5000/";
+export const API_BASE_URL = process.env.ASSETS_BASE_URL || "http://localhost:5000/api/";
+export const ASSETS_BASE_URL = process.env.API_BASE_URL ? (process.env.NEXT_PUBLIC_BASE_URL + "/") : "http://localhost:5000/";
 
 
